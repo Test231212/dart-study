ffbe62ceb0f68881091d10dc633720c426044c08
test231212 <j910219@gmail.com>
2024-04-05 14:55:34 +0900

    함수

---
+++ ch01/함수구구단.dart
@@ -0,0 +1,16 @@
+// 1~9단까지 출력해주는 함수
+void gugudan(int num) {
+  print("${num}*1=${num * 1}");
+  print("${num}*2=${num * 2}");
+  print("${num}*3=${num * 3}");
+  print("${num}*4=${num * 4}");
+  print("${num}*5=${num * 5}");
+  print("${num}*6=${num * 6}");
+  print("${num}*7=${num * 7}");
+  print("${num}*8=${num * 8}");
+  print("${num}*9=${num * 9}");
+}
+
+void main() {
+  gugudan(4);
+}
