c3bed23c1cab5e53db4f25d48e50bdb4227970a2
test231212 <j910219@gmail.com>
2024-04-08 16:58:41 +0900

    컬렉션

---
+++ ch01/set.dart
@@ -0,0 +1,20 @@
+// Dart에서 기본적으로 제공하는 라이블러리를 import 함
+import 'dart:math';
+
+void main() {
+  Set<int> lotto = {};
+
+  // Random클래스는 dart:math 라이브러리를 사용
+  Random r = Random();
+  lotto.add(r.nextInt(45) + 1);
+  lotto.add(r.nextInt(45) + 1);
+  lotto.add(r.nextInt(45) + 1);
+  lotto.add(r.nextInt(45) + 1);
+  lotto.add(r.nextInt(45) + 1);
+  lotto.add(r.nextInt(45) + 1);
+
+  print(lotto);
+
+  // toList() 함수를 사용하면 list타입으로 변경 가능
+  List<int> lottoList = lotto.toList();
+}
